4b15bdd9cbc46d707227493067fca387d224276b
Nick Colavita <nickcolavita88@gmail.com>
2015-09-03 11:28:05 -0400

    Initial Commit

---
+++ cronut.rb
@@ -0,0 +1,19 @@
+
+require 'sinatra'
+require 'mandrill'
+
+get '/home' do
+	erb: homepage
+end
+
+get '/gallery' do
+	erb: gallery
+end
+
+get '/order' do
+	erb: order
+end
+
+get '/review' do
+	erb: review
+end
