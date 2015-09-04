5f4d2f12e20c86a96976118c83f1d8a302402835
Nick Colavita <nickcolavita88@gmail.com>
2015-09-04 13:13:04 -0400

    Fixing Animation on jQuery elements to reacte properly. Still need to fix fade in of text.

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -3,31 +3,31 @@ $(document).ready(function(){
 //Creating a Jiggle Function to rotate the images
 	$.fn.jiggle = function(){
 
-		var times = 10;
+		var times = 6;
 		//console.log(currentDeg);
 		
 		for (var i = 1; i <= times; i++){
 			if(i % 2 === 0){
 				$(this).animate({
 					left: "+=10"
-				},1000);
+				},100);
 			}else{
 				$(this).animate({
 					left: "-=10"
-				},1000);
+				},100);
 			}
 		}
 	};
 
 
-	$("#shake").click(function(){
+	$(".shake").click(function(){
 		$(this).jiggle();
 	})
 
 
-//fade in text
-	// $(".galleryImages").click(function(){
-	// 	$(".text_fadein").fadeIn(2000);
-	// })
+// fade in text
+	$(".galleryImages").click(function(){
+		$(".text_fadein").delay(300).fadeIn(2000);
+	})
 
 })
