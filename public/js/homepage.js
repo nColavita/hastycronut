ce68189e4791c20cad318011110fa16be6b60aa2
Nick Colavita <nickcolavita88@gmail.com>
2015-09-06 09:43:32 -0400

    Fixed Animation on Homepage.

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,5 +1,22 @@
-$(document).ready(function (){
-	$(".donut").slideUp(7000);
-	$(".croissant").slideUp(8000);
-	$(".cronut").delay(1000).slideDown(9000);
-});
+// $(document).ready(function (){
+// 	$(".donut").slideUp(7000);
+// 	$(".croissant").slideUp(8000);
+// 	$(".cronut").delay(1000).slideDown(9000);
+// });
+
+$(document).ready(function(){
+
+	// $(window).load(function() {
+	// 	$(".cronut").delay(1000).show("slide", { direction: "down" }, 2000)
+	// });
+
+	
+		$(".donut").delay(500).show("slide", { direction: "left" }, 1000);
+
+
+		$(".croissant").delay(2000).show("slide", { direction: "right" }, 1000);
+
+		$(".cronut").delay(4000).show("slide", { direction: "down" }, 2000)
+
+
+})
