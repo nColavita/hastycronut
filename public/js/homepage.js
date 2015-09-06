7ec92e45729bcf9bbc31e5a7399e7a5147aa6693
Nick Colavita <nickcolavita88@gmail.com>
2015-09-06 09:56:38 -0400

    Tweaking timing on homepage Animation. Adding fade in and out effects to images.

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -13,10 +13,13 @@ $(document).ready(function(){
 	
 		$(".donut").delay(500).show("slide", { direction: "left" }, 1000);
 
-
 		$(".croissant").delay(2000).show("slide", { direction: "right" }, 1000);
 
-		$(".cronut").delay(4000).show("slide", { direction: "down" }, 2000)
+		$('#donutCrossaint_Master').delay(3000).fadeOut(2000);
+
+		$('.cronut').delay(5000).fadeIn(1500);
+
+		// $(".cronut").delay(4000).show("slide", { direction: "down" }, 1500)
 
 
 })
