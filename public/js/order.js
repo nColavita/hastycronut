9313e2876841c5a653b412c5a4bcc37afa4e88cf
amclean1 <lex213075@aim.com>
2015-09-07 10:37:24 -0400

    added cart functionality to the top of the order page for the customer to select what they are buying and what the total amount it

--- public/js/order.js
+++ public/js/order.js
@@ -2,5 +2,15 @@ $(document).ready(function() {
 	$(window).load(function() {
 		$(".form-style-6").show("slide", { direction: "right" }, 1000)
 	});
-})
+		
+
     
+	var theTotal = 0;
+	$('button').click(function(){
+	   theTotal = Number(theTotal) + Number($(this).val());
+	    $('.total').text("Total: $"+theTotal);        
+	});
+	
+	$('.total').text("Total: $"+theTotal);        
+
+})
